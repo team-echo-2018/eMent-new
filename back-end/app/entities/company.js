79d3c4f67ce3a4e7e2f45d0105b5e7950db56491
Sachintha <ravindusachintha53@gmail.com>
2018-10-31 07:15:15 +0530

    initialize backend entities

---
+++ back-end/app/entities/company.js
@@ -0,0 +1,12 @@
+var Company = function(companyId, companyName, companyAddress, companyPhone, companyEmail, companyDescription){
+
+    this.companyId = companyId;
+    this.companyName = companyName;
+    this.companyAddress = companyAddress;
+    this.companyPhone = companyPhone;
+    this.companyEmail = companyEmail;
+    this.companyDescription = companyDescription;
+
+}
+
+module.exports = Company;
