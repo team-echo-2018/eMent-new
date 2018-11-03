f6e21a0bfcd1d0089097c75ed91d7c4baee0b36d
Sachintha <ravindusachintha53@gmail.com>
2018-11-03 13:31:26 +0530

    is logged function update

--- front-end/src/app/pages/messaging/messaging.component.ts
+++ front-end/src/app/pages/messaging/messaging.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../../services/authentication.service';
 
 @Component({
   selector: 'app-messaging',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessagingComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthenticationService) { }
 
   ngOnInit() {
+    this.authService.isUserLogged();
   }
 
 }
