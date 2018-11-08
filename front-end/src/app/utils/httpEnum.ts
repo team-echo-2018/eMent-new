07ed1d8ced2aa9b36cae19e7d4e4bd22743854ad
Sachintha <ravindusachintha53@gmail.com>
2018-11-08 19:39:47 +0530

    file uploader ititialize

--- front-end/src/app/utils/httpEnum.ts
+++ front-end/src/app/utils/httpEnum.ts
@@ -1,6 +1,8 @@
 export enum HttpEnum {
+    BASEURL = 'http://localhost:8000/',
     ADDUSER = 'http://localhost:8000/api/addUser',
     AUTH    = 'http://localhost:8000/api/auth',
     GETUSER = 'http://localhost:8000/api/profile',
-    UPDATESTUDENT = 'http://localhost:8000/api/profile/update'
+    UPDATESTUDENT = 'http://localhost:8000/api/profile/update',
+    UPLOADFILE = 'http://localhost:8000/api/file/upload'
 }
