9075ea037daabb170015f6f68528ec9ce8e28f78
Sachini Chathurangi <chathurangi666@gmail.com>
2018-12-30 23:20:21 +0530

    changed project page

---
+++ front-end/src/app/components/working-proj/working-proj.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-working-proj',
+  templateUrl: './working-proj.component.html',
+  styleUrls: ['./working-proj.component.css']
+})
+export class WorkingProjComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
