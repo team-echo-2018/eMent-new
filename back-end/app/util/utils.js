3214613bc1799cbb7a1726f1000eda6b52e616cf
Sachintha <ravindusachintha53@gmail.com>
2019-01-01 17:46:08 +0530

    user, student, mentor sql utils defined

--- back-end/app/util/utils.js
+++ back-end/app/util/utils.js
@@ -3,6 +3,7 @@
  * Backend-Mysql-Database will call to here and this will call to entities.
  **/
 
+
 /* ---------  REQUIRES   -----------*/
 
 var Company = require('../entities/company');
@@ -20,6 +21,17 @@ var User = require('../entities/user');
 
 function Utils() { }
 
+
+
+// User Entity related functions
+// SQL query for select user by user name
+Utils.prototype.getSqlSelectUser = function (userName) {
+
+    var sqlSelectUser = "SELECT * FROM  users WHERE user_name like '" + userName + "'";
+    return sqlSelectUser;
+};
+
+// SQL query for insert user
 Utils.prototype.getInsertSqlUser = function (user) {
 
     var sqlUser = "INSERT INTO users (user_type, user_name, user_password) VALUES (" +
@@ -27,22 +39,57 @@ Utils.prototype.getInsertSqlUser = function (user) {
         "'" + user.userName + "', " +
         "'" + user.userPassword + "')";
     return sqlUser;
-}
-
-// SQL query for select user by user name
-Utils.prototype.getSqlSelectUser = function (userName) {
-
-    var sqlSelectUser = "SELECT * FROM  users WHERE user_name like '" + userName + "'";
-
-    return sqlSelectUser;
 };
 
+// SQL query for update user by id
+Utils.prototype.getUpdateSqlUser = function (user) {
+
+    var sqlUser = "UPDATE users SET" + 
+        " user_type='" + user.userType + "'," + 
+        " user_name='" + user.userName + "'," + 
+        " user_password='" + user.userPassword + "' WHERE" +
+        " user_id=" + user.userId;
+    return sqlUser;
+};
+
+// SQL query for delete user by id
+Utils.prototype.getSqlDeleteUser = function (user) {
+
+    var sqlDeleteUser = "DELETE FROM users WHERE user_id=" + user.userId;
+    return sqlDeleteUser;
+};
+
+
+
+// Student Entity related functions
 // SQL query for select student by student id
 Utils.prototype.getSqlSelectStudent = function (studentId) {
 
     var sqlSelectStudent = "SELECT * FROM student WHERE student_id=" + studentId;
-
     return sqlSelectStudent;
+};
+
+// SQL query for select student by student first name
+Utils.prototype.getSqlSelectStudentByFname = function (studentFname) {
+
+    var sqlSelectStudent = "SELECT * FROM  student WHERE student_fname like '" + studentFname + "'";
+    return sqlSelectStudent;
+};
+
+// SQL query for insert student
+Utils.prototype.getInsertSqlStudent = function (student) {
+
+    var sqlInsertStudent = "INSERT INTO student (student_id, student_fname, student_lname, " +
+        ", student_address, student_phone, student_email, student_imglink, student_description) VALUES (" +
+              student.studentId + ", " +
+        "'" + student.studentFname + "', " +
+        "'" + student.studentLname + "', " +
+        "'" + student.studentAddress + "', " +
+        "'" + student.studentPhone + "', " +
+        "'" + student.studentEmail + "', " +
+        "'" + student.studentImgLink + "', " +
+        "'" + student.studentDescription + "')";
+    return sqlInsertStudent;
 };
 
 // SQL query for update student by student id
@@ -57,16 +104,47 @@ Utils.prototype.getSqlUpdateStudent = function (student) {
         " student_imglink='" + student.studentImgLink + "'," +
         " student_description='" + student.studentDescription + "' WHERE" +
         " student_id=" + student.studentId;
-
     return sqlUpdateStudent;
 };
 
+// SQL query for delete student by id
+Utils.prototype.getSqlDeleteStudent = function (student) {
+
+    var sqlDeleteStudent = "DELETE FROM student WHERE student_id=" + student.studentId;
+    return sqlDeleteStudent;
+};
+
+
+
+// Mentor Entity related functions
 // SQL query for select mentor by mentor id
 Utils.prototype.getSqlSelectMentor = function (mentorId) {
 
     var sqlSelectMentor = "SELECT * FROM mentor WHERE mentor_id=" + mentorId;
-
     return sqlSelectMentor;
+};
+
+// SQL query for select mentor by mentor first name
+Utils.prototype.getSqlSelectMentorByFname = function (mentorFname) {
+
+    var sqlSelectMentor = "SELECT * FROM  mentor WHERE mentor_fname like '" + mentorFname + "'";
+    return sqlSelectMentor;
+};
+
+// SQL query for insert mentor
+Utils.prototype.getInsertSqlMentor = function (mentor) {
+
+    var sqlInsertMentor = "INSERT INTO mentor (mentor_id, mentor_fname, mentor_lname, " +
+        ", mentor_address, mentor_phone, mentor_email, mentor_imglink, mentor_description) VALUES (" +
+              mentor.mentorId + ", " +
+        "'" + mentor.mentorFname + "', " +
+        "'" + mentor.mentorLname + "', " +
+        "'" + mentor.mentorAddress + "', " +
+        "'" + mentor.mentorPhone + "', " +
+        "'" + mentor.mentorEmail + "', " +
+        "'" + mentor.mentorImgLink + "', " +
+        "'" + mentor.mentorDescription + "')";
+    return sqlInsertMentor;
 };
 
 // SQL query for update mentor by mentor id
@@ -81,10 +159,20 @@ Utils.prototype.getSqlUpdateMentor = function (mentor) {
         " mentor_imglink='" + mentor.mentorImgLink + "'," +
         " mentor_description='" + mentor.mentorDescription + "' WHERE" +
         " mentor_id=" + mentor.mentorId;
-
     return sqlUpdateMentor;
 };
 
+// SQL query for delete mentor by id
+Utils.prototype.getSqlDeleteMentor = function (mentor) {
+
+    var sqlDeleteMentor = "DELETE FROM mentor WHERE mentor_id=" + mentor.mentorId;
+    return sqlDeleteMentor;
+};
+
+
+
+
+// Objects generating function definitions
 // user object generating function
 Utils.prototype.generateUser = function (resultUser) {
     var user = null;
@@ -132,5 +220,5 @@ Utils.prototype.generateMentor = function (resultMentor) {
     return mentor;
 }
 
-// export utils function
+// export utils
 module.exports = Utils;
