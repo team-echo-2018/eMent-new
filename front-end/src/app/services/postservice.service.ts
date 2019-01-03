c0d896d99d09ab6ab790eae0a0e9dd5621dadca5
marioperera <pereramario19@gmail.com>
2019-01-03 12:25:00 +0530

    deletePosts line 35

--- front-end/src/app/services/postservice.service.ts
+++ front-end/src/app/services/postservice.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Posts } from '../entities/posts';
+import { HttpEnum } from '../utils/httpEnum';
+import { HttpBackendRequestService } from './http-backend-request.service';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -12,7 +14,30 @@ const httpOptions = {
 })
 export class PostserviceService {
 
-  constructor( private http: HttpClient) { }
+  constructor( private httpBackendRequest: HttpBackendRequestService) { }
+
+  getPosts(){
+    this.httpBackendRequest.realizarHttpPost(HttpEnum.BASEURL +"getPost",null).subscribe(
+      (results)=>{
+        if(results ==null){
+          console.log("error in getting posts --no posts");
+
+        }else{
+          return results;
+        }
+
+      }
+    )
+  }
+  deletePosts(Postdetails:Posts){
+    this.httpBackendRequest.realizarHttpPost(HttpEnum.BASEURL+"deletePost",Postdetails).subscribe(
+      (error)=>{
+        console.log("porblem deleting posts");
+
+      }
+
+    )
+  }
 
 
 }
