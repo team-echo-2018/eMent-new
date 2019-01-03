298b7bebdb397eaf94d41049ab68e797c8ea2d56
marioperera <pereramario19@gmail.com>
2019-01-03 11:33:21 +0530

    test

--- front-end/src/app/services/postservice.service.ts
+++ front-end/src/app/services/postservice.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Posts } from '../entities/posts';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostserviceService {
 
-  constructor() { }
+  constructor( private http: HttpClient) { }
+
+
 }
