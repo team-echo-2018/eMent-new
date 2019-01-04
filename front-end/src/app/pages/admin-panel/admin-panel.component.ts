b24574f84b809b825f607557dd9ff11af2161023
Sachintha <ravindusachintha53@gmail.com>
2019-01-04 16:38:09 +0530

    update admin panel content

--- front-end/src/app/pages/admin-panel/admin-panel.component.ts
+++ front-end/src/app/pages/admin-panel/admin-panel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../../services/authentication.service';
 
 @Component({
   selector: 'app-admin-panel',
@@ -7,9 +8,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminPanelComponent implements OnInit {
 
-  constructor() { }
+  dashboardActive:boolean;
+  companiesActive: boolean;
+  mentorsActive:boolean;
+  studentsActive: boolean;
+  notificationsActive:boolean;
+  title: string;
+
+  constructor(private authService: AuthenticationService) { }
 
   ngOnInit() {
+    this.activateDashboard();
+  }
+
+  // page contents managing functions
+  activateDashboard() {
+    this.dashboardActive = true;
+    this.companiesActive = false;
+    this.mentorsActive = false;
+    this.studentsActive = false;
+    this.notificationsActive = false;
+    this.title = "Dashboard";
+  }
+
+  activateCompanies() {
+    this.dashboardActive = false;
+    this.companiesActive = true;
+    this.mentorsActive = false;
+    this.studentsActive = false;
+    this.notificationsActive = false;
+    this.title = "Companies";
+  }
+
+  activateMentors() {
+    this.dashboardActive = false;
+    this.companiesActive = false;
+    this.mentorsActive = true;
+    this.studentsActive = false;
+    this.notificationsActive = false;
+    this.title = "Mentors";
+  }
+
+  activateStudents() {
+    this.dashboardActive = false;
+    this.companiesActive = false;
+    this.mentorsActive = false;
+    this.studentsActive = true;
+    this.notificationsActive = false;
+    this.title = "Students";
+  }
+
+  activateNotifications() {
+    this.dashboardActive = false;
+    this.companiesActive = false;
+    this.mentorsActive = false;
+    this.studentsActive = false;
+    this.notificationsActive = true;
+    this.title = "Notifications";
+  }
+
+
+  // logout funtion
+  logout() {
+    this.authService.logoutUser();
   }
 
 }
