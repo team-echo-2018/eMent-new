b1b0e41a6ac2dba91d331874168feea8dc0875cc
marioperera <pereramario19@gmail.com>
2019-01-06 07:59:51 +0530

    tests added

--- front-end/src/app/components/chat/chat.component.spec.ts
+++ front-end/src/app/components/chat/chat.component.spec.ts
@@ -1,10 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ChatComponent } from './chat.component';
+import { DebugElement } from '@angular/core';
 
 describe('ChatComponent', () => {
   let component: ChatComponent;
   let fixture: ComponentFixture<ChatComponent>;
+  let de:DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
