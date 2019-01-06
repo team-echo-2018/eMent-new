ff148cf24ec04d874d075174998398090c18c305
marioperera <pereramario19@gmail.com>
2019-01-06 15:53:00 +0530

    commit 41

--- front-end/src/app/components/chat/chat.component.ts
+++ front-end/src/app/components/chat/chat.component.ts
@@ -66,6 +66,7 @@ export class ChatComponent implements OnInit {
     this.senderId =senderID;
     console.log("user selected "+this.senderId);
 
+    this.messageCol =this.afs.collection('messages',ref => ref.where('recieverID','==',this.senderId).where('senderID','==',this.owner));
     this.messageCol =this.afs.collection('messages',ref => ref.where('recieverID','==',this.owner).where('senderID','==',this.senderId));
     this.messages =this.messageCol.valueChanges();
     this.messages.subscribe(
@@ -84,31 +85,19 @@ export class ChatComponent implements OnInit {
 
       }
     )
-    //this.data.changesMessage(senderID);
   }
 
-  //THE PART FOR THE ADDITION OF NEW MESSAGES TO THE FIRESTORE
-  // THERE ARE CURRENTLY TWO MAIN DATABSE COLLECTIONS *sendEvent* FOR MAINTAINING THE DETAILS OF A MESSAGE BEING SENT
-  // * messages * FOR RECORDING THE DETAILS OF MESSAGES BEING SENT
 
 
   addmessage() {
 
-    // *** CHECK FOR DUPLICATES OF PRIOR SENDEVENTS FROM sendEvent COLLECTION ***
 
-    let duplicates =this.afs.collection('sendEvent',ref =>ref.where('recieverID','==',this.owner).where("senderID","==",this.senderId));
 
-    // ADD NEW MESSAGE DOCUMENT TO message COLLECTION
+    let duplicates =this.afs.collection('sendEvent',ref =>ref.where('recieverID','==',this.senderId).where("senderID","==",this.owner));
 
-    //                      $$$$$$ IMPORTANT $$$$$$
 
-    //CHANGE THE VALUES OF this.recieverID and this.senderID AS THESE ARE FOR TESTING PERPOSES
-    //    this.senderID ==> this.owner
-    //    this.recieverID ==> this.reciverId
+    this.afs.collection('messages').add({'message':this.message,'recieverID':this.recieverid,'senderID':this.owner});
 
-    this.afs.collection('messages').add({'message':this.message,'recieverID':this.recieverid,'senderID':this.senderId});
-
-    //IF DUPLICATES DONT EXIST AND THE #duplicate VARIABLE IS NONE ==>PROCEED!!
 
     if(!duplicates){
 
@@ -121,7 +110,7 @@ export class ChatComponent implements OnInit {
     }
     this.messagelist.push(Utils.convertimessagetomessage({'message':this.message,'recieverID':this.recieverid,'senderID':this.senderId}));
 
-
+    alert("message added");
 
 
 }
