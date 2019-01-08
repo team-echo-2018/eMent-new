59903e70d16b7d949bd32c437279d3f3b4c3a89e
marioperera <pereramario19@gmail.com>
2019-01-08 21:28:20 +0530

    commit

--- front-end/src/app/services/nortification.service.ts
+++ front-end/src/app/services/nortification.service.ts
@@ -10,11 +10,18 @@ import { nortification} from '../entities/nortification';
   providedIn: 'root'
 })
 export class NortificationService {
-  noritfications:nortification[] =new Array();
+  noritfications:nortification[];
 
   constructor( private httpBackendRequest: HttpBackendRequestService,private Http :HttpClient) { }
 
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.noritfications =new Array();
+    this.noritfications =this.getnortifications();
+  }
   getnortifications(){
+    this.noritfications =new Array();
     console.log("get nortifications service called");
 
     this.httpBackendRequest.realizarHttpPost(HttpEnum.GETNORTIFICATION,null).subscribe(
